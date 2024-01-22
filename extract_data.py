5b8437f41fbc8bf4702b2076c7f089f569ee9202
Rodrigo Leão <rodrigo.leao@radixeng.com.br>
2024-01-22 19:23:51 -0300

    script to obtain data

--- extract_data.py
+++ extract_data.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pandas as pd
 from time import sleep
+from datetimeHelper import *
+import pandas as pd
+import math
 
-#URL = "https://vale.com/pt/dividendos-dividas-e-debentures"
-URL = "https://investidor10.com.br/acoes/@ticker/"
 ticker = "SANB3"
 
 
@@ -16,7 +16,9 @@ options.add_argument("--disable-cache")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
 
 driver = webdriver.Chrome(options=options)
-
+"""
+# Histórico de dividendos (Investidor10)
+URL = "https://investidor10.com.br/acoes/@ticker/"
 driver.get(URL.replace("@ticker", ticker))
 
 tableElem = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//*[@id='table-dividends-history']")))
@@ -41,4 +43,57 @@ for page in range(len(navBar[0].text.split("\n")) - 1) :
 
     driver.find_element(By.XPATH, "//*[@id='table-dividends-history_next']").click()
 
-df.to_csv(f"dividends_history_{ticker}_report.csv", index=False, sep=";")
+"""
+
+df = pd.read_csv(f'reports/dividends_history_{ticker}_report.csv', sep=";")
+df.columns = ["Tipo", "Data Com", "Data Ex", "Valor", "Data Com Preço", "Data Ex Preço"]
+    
+df = df.reset_index()
+df = df.drop(["index"], axis=1)
+    
+if not "Data Com Preço" in df:
+    df["Data Com Preço"] = ""
+
+if not "Data Ex Preço" in df:
+    df["Data Ex Preço"] = ""
+
+# Histórico de preço (Yahoo Finanças)
+try:
+    for index, row in df.iterrows():
+        if "Data Com Preço" in row and "float" in str(type(row["Data Com Preço"])):
+            URL = "https://www.ibovx.com.br/historico-papeis-bovespa.aspx?papel=@ticker&dtini=@date&dtfim=@date"
+            
+            for col in ["Data Com", "Data Ex"]:
+                if convertStrToDate(row[col]) > datetime.now():
+                    continue
+
+                if convertStrToDate(row[col]) < datetime(2000,1,1):
+                    continue
+
+                driver.get(URL.replace("@ticker", ticker).replace("@date", row[col]))
+
+                tableElem = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//*[@id='idConteudo']/div/div[9]/table")))
+                tableContent = tableElem.text.split("\n")[1:]
+                tableContentDividend = [x.split(" ") for x in tableContent]
+
+                lineRow = [x for x in tableContentDividend if x[0] == row[col]]
+
+                if len(lineRow) == 0:
+                    lineRow = [x for x in tableContentDividend if x[0][3:] == row[col][3:] ]
+
+                if len(lineRow) == 0:
+                    lineRow = tableContentDividend[0]
+
+                
+
+                #lineRow = next(filter(lambda x: x[0] == row[col], tableContentDividend))
+                dayEndPrice = lineRow[0][3]
+                row[col + " Preço"] = dayEndPrice
+                
+
+            df.loc[row.name] = row
+            print()
+except:    
+    df.to_csv(f"reports/dividends_history_{ticker}_report.csv", index=False, sep=";")
+
+df.to_csv(f"reports/dividends_history_{ticker}_report.csv", index=False, sep=";")
